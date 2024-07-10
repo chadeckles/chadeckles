0d10b62b18362d9761afc35def7ae9eb63edf266
Chad Eckles <chadeckles@github.com>
2024-07-10 12:37:45 -0600

    Update HackathonDemo.tests.ps1

--- tests/HackathonDemo.tests.ps1
+++ tests/HackathonDemo.tests.ps1
@@ -16,7 +16,7 @@
 # 4.) PROFIT!
 #
 
-Sample Test - Verify Allowed Locations 
+#Sample Test - Verify Allowed Locations 
 Describe "Validate Allowed Locations Policy" {
     It "Checking to see if Azure Policy blocks the deployment of a resource in a disallowed location" {
        { az vm create --resource-group chadeckles --name PesterTestVM --image Ubuntu2204 --size Standard_B1s --location spaincentral --generate-ssh-keys  
